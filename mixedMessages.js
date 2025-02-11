23f7fcc944b2c494bee0a547ed4d837b07e175f0
Jacob <program.acquaint533@passinbox.com>
2025-02-11 16:34:13 +1100

    Added the functions necessary to create the random sentences

--- mixedMessages.js
+++ mixedMessages.js
@@ -7,6 +7,20 @@ console.clear();
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
 
+// Create a set of random words to create sentences
+const subjects = ["The cat", "A programmer", "My neighbor", "The astronaut", "A curious dog"];
+const actions = ["jumps over", "writes code for", "bakes", "paints", "fixes"];
+const objects = ["a giant cake.", "a new website.", "the morning newspaper.", "a mysterious door.", "a flying car."];
+
+// Func takes an array and replies with a random word
+function getRandomWord(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function makeTheSentence() {
+  return `${getRandomWord(subjects)} ${getRandomWord(actions)} ${getRandomWord(objects)}`;
+}
+
 console.log("Welcome to mixed messages.");
 console.log("A program to generate random sentences.")
 console.log("To exit the program at any time, simply hit the enter key.");
@@ -14,9 +28,9 @@ console.log("\nPress Space to generate a message, or Enter to exit.");
 
 process.stdin.on('keypress', (str, key) => {
     if (key.name === 'space') {
-        console.log("🎲 Random message will appear here!"); // Placeholder for actual messages
+    	console.log(makeTheSentence());
     } else if (key.name === 'return') {
-        console.log("Goodbye!");
-        process.exit();
+      console.log("Goodbye!");
+      process.exit();
     }
 });

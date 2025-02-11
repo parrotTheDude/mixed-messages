7f789af4c79289372d6159df4e76b3dd5e27c865
Jacob <program.acquaint533@passinbox.com>
2025-02-11 16:15:55 +1100

    Initial commit and basic text to describe what the program is to the user

---
+++ mixedMessages.js
@@ -0,0 +1,2 @@
+console.log("Welcome to mixed messages. The intention of this project is to simply generate a random sentence every time you the user hits the space bar.")
+console.log("To exit the program at anytime, simply hit the enter key.")
